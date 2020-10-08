df18848e838664fa6a0e3edb893f5ae14b1c69dd
SJY <46434838+ebbunnim@users.noreply.github.com>
2020-10-08 11:07:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -23,26 +23,3 @@ def get_svg(request):
     plt.cla() # clean up plt so it can be re-used
     response = HttpResponse(svg, content_type='image/svg+xml')
     return response
-
-
-# from django.http import HttpResponse
-# import random
-# import numpy as np
-# from matplotlib.backends.backend_svg import FigureCanvas
-# from matplotlib.figure import Figure
-
-# def getFig():
-#     fig=Figure(facecolor='w')
-#     ax=fig.add_subplot(111)
-#     numPts = 50
-#     x = [random.random() for n in range(numPts)]
-#     y = [random.random() for n in range(numPts)]
-#     sz = 2 ** (10*np.random.rand(numPts))
-#     ax.scatter(x, y, s=sz, alpha=0.5)
-#     return fig
-
-# def get_svg(request):
-#     fig = getFig()
-#     response = HttpResponse(content_type='image/svg+xml')
-#     FigureCanvas(fig).print_svg(response)
-#     return response
